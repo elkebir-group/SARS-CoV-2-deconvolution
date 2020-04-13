b73d2eb35e1d47e2f026bb1bace487067ab53205
Palash Sashittal <sashitt2@illinois.edu>
2020-04-12 23:32:35 -0500

    Correct solver

--- src/solvergradientb.h
+++ src/solvergradientb.h
@@ -58,85 +58,22 @@ protected:
     double operator()(const Eigen::VectorXd& b,
                       Eigen::VectorXd& grad)
     {
-      //      using namespace boost::numeric::ublas;
-      //
-      //      const int nrMutations = _F.size1();
-      //      const int nrStrains = _U.size1();
-      //      BoostDoubleMatrix B(nrMutations, nrStrains, 0);
-      //      for (int ii = 0; ii < _n; ++ii)
-      //      {
-      //        int i = ii / nrStrains;
-      //        int j = ii % nrStrains;
-      //        B(i, j) = b[ii];
-      //      }
-      //
-      //      BoostDoubleMatrix BU = prod(B, _U);
-      //      const int nrSamples = _U.size2();
-      //
-      //      // compute the objective function and the gradient
-      //      double fb = 0;
-      //      for (int i = 0; i < nrMutations; ++i)
-      //      {
-      //        for (int j = 0; j < nrStrains; ++j)
-      //        {
-      //          double bij = B(i,j);
-      //          int ii = i * nrStrains + j;
-      //          double sign = ( bij - bij * bij > 0 ) * 2.0 - 1.0;
-      //
-      //          if ( fabs(bij - bij * bij) < 1e-4 ) sign = 0;
-      //
-      //          // penalty term in the gradient
-      //          grad[ii] = _lambda * sign * ( 1 - 2 * bij );
-      //
-      //          for (int p = 0; p < nrSamples; ++p)
-      //          {
-      //            if (_F(i,p) != -1)
-      //            {
-      //              // update gradient if F is not nan
-      //              grad[ii] += 2 * BU(i,p) * _U(j,p) - 2 * _F(i,p) * _U(j,p);
-      //            }
-      //            else
-      //            {
-      //              std::cout << "here" << std::endl;
-      //            }
-      //          }
-      //
-      //          fb += _lambda * fabs(bij * bij - bij);
-      //        }
-      //      }
-      //
-      //      for (int i = 0; i < nrMutations; ++i)
-      //      {
-      //        for (int p = 0; p < nrSamples; ++p)
-      //        {
-      //          if (_F(i,p) != -1)
-      //          {
-      //            // update objective function if F is not nan
-      //            fb += (_F(i,p) - BU(i,p)) * (_F(i,p) - BU(i,p));
-      //          }
-      //        }
-      //      }
-      //
-      //      return fb;
-      
       using namespace boost::numeric::ublas;
       
       const int nrMutations = _F.size1();
       const int nrStrains = _U.size1();
-      
       BoostDoubleMatrix B(nrMutations, nrStrains, 0);
       for (int ii = 0; ii < _n; ++ii)
       {
         int i = ii / nrStrains;
         int j = ii % nrStrains;
-        
         B(i, j) = b[ii];
       }
       
-      BoostDoubleMatrix B_U = prod(B, _U);
-      BoostDoubleMatrix M_BU_F = element_prod(_M, B_U - _F);
-      BoostDoubleMatrix M_BU_F_Ut = prod(M_BU_F, _Ut);
+      BoostDoubleMatrix BU = prod(B, _U);
+      const int nrSamples = _U.size2();
       
+      // compute the objective function and the gradient
       double fb = 0;
       for (int i = 0; i < nrMutations; ++i)
       {
@@ -145,18 +82,81 @@ protected:
           double bij = B(i,j);
           int ii = i * nrStrains + j;
           double sign = ( bij - bij * bij > 0 ) * 2.0 - 1.0;
-          
-          if ( abs(bij - bij * bij) < 1e-4 ) sign = 0;
-          
-          grad[ii] = 2 * M_BU_F_Ut(i, j) + _lambda * sign * ( 1 - 2 * bij );
-          
-          fb += _lambda * abs(bij * bij - bij);
+      
+          if ( fabs(bij - bij * bij) < 1e-4 ) sign = 0;
+      
+          // penalty term in the gradient
+          grad[ii] = _lambda * sign * ( 1 - 2 * bij );
+      
+          for (int p = 0; p < nrSamples; ++p)
+          {
+            if (_F(i,p) != -1)
+            {
+              // update gradient if F is not nan
+              grad[ii] += 2 * BU(i,p) * _U(j,p) - 2 * _F(i,p) * _U(j,p);
+            }
+            else
+            {
+              std::cout << "here" << std::endl;
+            }
+          }
+      
+          fb += _lambda * fabs(bij * bij - bij);
         }
       }
       
-      fb += pow(norm_frobenius(M_BU_F), 2.);
+      for (int i = 0; i < nrMutations; ++i)
+      {
+        for (int p = 0; p < nrSamples; ++p)
+        {
+          if (_F(i,p) != -1)
+          {
+            // update objective function if F is not nan
+            fb += (_F(i,p) - BU(i,p)) * (_F(i,p) - BU(i,p));
+          }
+        }
+      }
       
       return fb;
+      
+      //using namespace boost::numeric::ublas;
+      //
+      //const int nrMutations = _F.size1();
+      //const int nrStrains = _U.size1();
+      //
+      //BoostDoubleMatrix B(nrMutations, nrStrains, 0);
+      //for (int ii = 0; ii < _n; ++ii)
+      //{
+      //  int i = ii / nrStrains;
+      //  int j = ii % nrStrains;
+      //  
+      //  B(i, j) = b[ii];
+      //}
+      //
+      //BoostDoubleMatrix B_U = prod(B, _U);
+      //BoostDoubleMatrix M_BU_F = element_prod(_M, B_U - _F);
+      //BoostDoubleMatrix M_BU_F_Ut = prod(M_BU_F, _Ut);
+      //
+      //double fb = 0;
+      //for (int i = 0; i < nrMutations; ++i)
+      //{
+      //  for (int j = 0; j < nrStrains; ++j)
+      //  {
+      //    double bij = B(i,j);
+      //    int ii = i * nrStrains + j;
+      //    double sign = ( bij - bij * bij > 0 ) * 2.0 - 1.0;
+      //    
+      //    if ( abs(bij - bij * bij) < 1e-4 ) sign = 0;
+      //    
+      //    grad[ii] = 2 * M_BU_F_Ut(i, j) + _lambda * sign * ( 1 - 2 * bij );
+      //    
+      //    fb += _lambda * abs(bij * bij - bij);
+      //  }
+      //}
+      //
+      //fb += pow(norm_frobenius(M_BU_F), 2.);
+      //
+      //return fb;
       
       //      std::ofstream outGrad("outgrad.txt");
       //      for (int i = 0; i < nrMutations; ++i)
